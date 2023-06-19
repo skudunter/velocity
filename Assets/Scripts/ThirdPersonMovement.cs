845ead7b2a3d92160335fd1fb48dfe9cfac6779e
skudunter <danielgruttercpt@gmail.com>
2023-06-19 20:10:01 +0200

    feat added playercontroller

---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController _characterController;
+
+    [SerializeField]
+    private float _speed = 5f;
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+        _characterController.Move(direction * _speed * Time.deltaTime);
+    }
+}
